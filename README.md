5198a67356811ee6abac3c953fbec8aefc8ef1ce
Nils Fahldieck <nils.fahldieck@gmail.com>
2022-06-22 12:01:08 +0200

    Add README

---
+++ README.md
@@ -0,0 +1,12 @@
+# free-domain-scanner
+
+Scanner to check for available short *.de domains.
+
+## TODO
+
+- [ ] Make runes creation a function
+- [ ] Brute Force Algo, 1-3 chars. Permutation
+- [ ] buffer results
+- [ ] Threading
+- [ ] Threading: print no of domains every second
+- [ ] Cobra for own charset
--- main.go
+++ main.go
@@ -8,12 +8,6 @@ import (
 )
 
 func main() {
-	// TODO: Brute Force Algo, 1-3 chars. Permutation
-	// TODO: buffer results
-	// TODO: Threading
-	// TODO: Threading: print no of domains every second
-	// TODO: Cobra for own charset
-
 	// all lower-case ASCII alnum runes
 	var runes []rune
 	for r := 'a'; r <= 'z'; r++ {
